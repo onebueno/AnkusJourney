b1b881b4a883eb8c26bee62b65597c6a20b68d81
Quizz02 <56943552+Quizz02@users.noreply.github.com>
2019-11-08 00:19:53 -0500

    Update ArregloBalas.h

--- ArregloBalas.h
+++ ArregloBalas.h
@@ -3,6 +3,7 @@
 #include "Bala.h"	
 #include <vector>
 
+using namespace System::Drawing;
 using namespace std;
 
 class ArregloBalas {
@@ -13,12 +14,12 @@ public:
 	~ArregloBalas() {}
 
 	void agregarbala(Heroe * heroe) {
-		Bala *aux = new Bala(heroe->getx(), heroe->gety());
+		Bala *aux = new Bala(heroe->getx()+90, heroe->gety()+30);
 		arr_balas.push_back(aux);
 	}
 
-	void moverbalas(BufferedGraphics^ buffer, Bitmap^ bmp) {
+	void moverbalas(BufferedGraphics^ buffer,Bitmap ^bmp ,Heroe * heroe) {
 		for (int i = 0; i < arr_balas.size(); i++)
-			arr_balas.at(i)->Mover(buffer, bmp);
+			arr_balas.at(i)->Mover(buffer, bmp,heroe);
 	}
 };
