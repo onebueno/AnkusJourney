a714b63c6f3b927bae60dd9b78837b62e16d8da0
Quizz02 <56943552+Quizz02@users.noreply.github.com>
2019-11-07 19:35:22 -0500

    Update AnkusJourney.h

--- AnkusJourney.h
+++ AnkusJourney.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Heroe.h"
+#include "Alfa.h"
 
 namespace AJ {
 
@@ -9,6 +10,7 @@ namespace AJ {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Media;
 
 	/// <summary>
 	/// Resumen de AnkusJourney
@@ -17,6 +19,7 @@ namespace AJ {
 	{
 	private:
 		Heroe* objheroe;
+		Alfa* objAlfa;
 		Bitmap^ bmpparado;
 		Bitmap^ bmpderecha;
 		Bitmap^ bmpizquierda;
@@ -25,6 +28,11 @@ namespace AJ {
 		Bitmap^ contaminacion;
 		Bitmap^ criminal;
 		Bitmap^ criminal2;
+		Bitmap^ trashcan;
+		Bitmap^ ataque;
+		Graphics ^g;
+		BufferedGraphicsContext^ espacio;
+		BufferedGraphics^ buffer;
 		int nivel;
 	public:
 		AnkusJourney(void)
@@ -32,6 +40,7 @@ namespace AJ {
 			//TODO: agregar código de constructor aquí
 			InitializeComponent();
 			objheroe = new Heroe(50, 400);
+			objAlfa = new Alfa(800, 500);
 			bmpparado = gcnew Bitmap("parado.png");
 			bmpderecha = gcnew Bitmap("movederecha.png");
 			bmpizquierda = gcnew Bitmap("moveizquierda.png");
@@ -40,6 +49,11 @@ namespace AJ {
 			contaminacion = gcnew Bitmap("pollution.png");
 			criminal = gcnew Bitmap("criminal.png");
 			criminal2 = gcnew Bitmap("criminal2.png");
+			trashcan = gcnew Bitmap("trashcanleft.png");
+			ataque = gcnew Bitmap("atacar.png");
+			//g = this->CreateGraphics();
+			//espacio = BufferedGraphicsManager::Current;
+			//buffer = espacio->Allocate(g, this->ClientRectangle);
 			nivel = 1;
 		}
 
@@ -84,7 +98,7 @@ namespace AJ {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1081, 554);
+			this->ClientSize = System::Drawing::Size(1079, 551);
 			this->Name = L"AnkusJourney";
 			this->Text = L"AnkusJourney";
 			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &AnkusJourney::presionartecla);
@@ -94,13 +108,17 @@ namespace AJ {
 		}
 #pragma endregion
 	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
-		
-		Graphics ^g = this->CreateGraphics();
-		BufferedGraphicsContext^ espacio = BufferedGraphicsManager::Current;
-		BufferedGraphics^ buffer = espacio->Allocate(g, this->ClientRectangle);
+
+		//Graphics ^
+		g = this->CreateGraphics();
+		//BufferedGraphicsContext^ 
+		espacio = BufferedGraphicsManager::Current;
+		//BufferedGraphics^ 
+		buffer = espacio->Allocate(g, this->ClientRectangle);
 		buffer->Graphics->Clear(Color::White);
 		if (nivel == 1) {
 			buffer->Graphics->DrawImage(contaminacion, 0, -50, contaminacion->Width*1.55, contaminacion->Height*1.3);
+			objAlfa->Mover(buffer, trashcan);
 		}
 		if (nivel == 2) {
 			buffer->Graphics->DrawImage(criminal, -45, -85, criminal->Width, criminal->Height);
@@ -108,28 +126,33 @@ namespace AJ {
 		if (nivel == 3) {
 			buffer->Graphics->DrawImage(corrupcion, 0, 0, corrupcion->Width*0.60, corrupcion->Height*0.50);
 		}
-		/*if (nivel == 4) {
-			buffer->Graphics->DrawImage(criminal2, 0, 0, criminal2->Width*1.72, criminal2->Height*1.7);
-		}*/
-		objheroe->mover(buffer, bmpizquierda, bmpderecha, bmpparado, bmpparadoizq,g);
+		objheroe->mover(buffer, bmpizquierda, bmpderecha, bmpparado, bmpparadoizq, ataque, g);
 		buffer->Render(g);
+
 		delete buffer;
 		delete espacio;
 		delete g;
 	}
 	private: System::Void soltartecla(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e) {
+		if (e->KeyCode == Keys::P)
+		{
+			objheroe->direccion = Direcciones::Ataque;
+		}
 		objheroe->direccion = Direcciones::Ninguna;
 	}
 	private: System::Void presionartecla(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e) {
 		switch (e->KeyCode) {
-		case Keys::Left:
+		case Keys::A:
 			objheroe->direccion = Direcciones::Izquierda;
 			break;
-		case Keys::Right:
+		case Keys::D:
 			objheroe->direccion = Direcciones::Derecha;
 			break;
+		case Keys::P:
+			objheroe->direccion = Direcciones::Ataque;
 
 		}
 	}
 	};
 }
+
