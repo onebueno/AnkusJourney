bf602eeba5792150bee6705518ed17bee0bee961
Quizz02 <56943552+Quizz02@users.noreply.github.com>
2019-11-10 19:01:35 -0500

    Controladora.h
    Cambié el nombre de arreglobalas.h a controladora.h

---
+++ Controladora.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "Heroe.h"
+#include "Bala.h"
+#include "Alfa.h"
+#include <vector>
+
+using namespace System::Drawing;
+using namespace std;
+
+class CControladora {
+private:
+	vector<Bala*>arr_balas;
+	vector<Alfa*>arr_alfa;
+	int num_a;
+public:
+	CControladora() {
+		num_a = 7 + rand() % 3;
+	}
+	~CControladora() {}
+
+	void agregarbala(Heroe * heroe) {
+		Bala *aux = new Bala(heroe->getx()+90, heroe->gety()+30);
+		arr_balas.push_back(aux);
+	}
+
+	void movertodo(BufferedGraphics^ buffer,Bitmap ^bmp,Bitmap ^izq) {
+		for (int i = 0; i < arr_balas.size(); i++)
+			arr_balas.at(i)->Mover(buffer, bmp);
+		for (int j = 0; j < arr_alfa.size(); j++)
+			arr_alfa.at(j)->Mover(buffer, izq);
+	}
+
+	void agregaralfa() {
+		for (int i = 0; i < num_a; i++) {
+			Alfa *aux = new Alfa(300+rand() % 600, 390 + rand() % 40);
+			arr_alfa.push_back(aux);
+		}
+	}
+
+	void colision(Graphics^ g) {
+		for (int i = 0; i < arr_balas.size(); i++) {
+			for (int j = 0; j < arr_alfa.size(); j++) {
+				if (arr_balas.at(i)->getBala().IntersectsWith(arr_alfa.at(j)->getalfa())) {
+					arr_balas.at(i)->setvisible(false);
+					arr_alfa.at(j)->setvisible(false);
+				}
+			}
+		}
+
+		for (int i = 0; i < arr_balas.size(); i++) {
+			if (arr_balas.at(i)->getX() <= 0 || arr_balas.at(i)->getX() >= g->VisibleClipBounds.Width ||
+				arr_balas.at(i)->gety() <= 0 || arr_balas.at(i)->gety() >= g->VisibleClipBounds.Height)
+			{
+				arr_balas.at(i)->setvisible(false);
+			}
+		}
+
+		for (int i = 0; i < arr_balas.size(); i++)
+		{
+			if (!arr_balas.at(i)->getvisible())
+			{
+				arr_balas.erase(arr_balas.begin() + i);
+			}
+		}
+
+		for (int i = 0; i < arr_alfa.size(); i++)
+		{
+			if (!arr_alfa.at(i)->getvisible())
+			{
+				arr_alfa.erase(arr_alfa.begin() + i);
+			}
+		}
+
+	}
+
+};
