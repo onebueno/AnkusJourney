dfe37b3989e139fec3e5bc71b80819b0a29d9011
Quizz02 <56943552+Quizz02@users.noreply.github.com>
2019-11-16 01:51:04 -0500

    Update Alfa.h

--- Alfa.h
+++ Alfa.h
@@ -1,13 +1,13 @@
 #pragma once
 #include "Enemigo.h"
 
-class Alfa: public Enemigo
+class Alfa : public Enemigo
 {
 public:
-	Alfa(int px,int py);
+	Alfa(int px, int py);
 	~Alfa();
-	void Dibujar(BufferedGraphics^ buffer, Bitmap^ bmp);
-	void Mover(BufferedGraphics^ buffer, Bitmap^ izq);
+	void Dibujar(BufferedGraphics^ buffer, Bitmap^ bmp, Bitmap^ der);
+	void Mover(BufferedGraphics^ buffer, Bitmap^ izq, Bitmap^ der);
 	Rectangle getalfa();
 	void setvisible(bool v);
 	bool getvisible();
@@ -21,7 +21,7 @@ Alfa::Alfa(int px, int py)
 	dy = 0;
 	indicex = 0;
 	indicey = 0;
-	ancho = 349/16;
+	ancho = 349 / 16;
 	alto = 18;
 	visible = true;
 }
@@ -30,23 +30,49 @@ Alfa::~Alfa()
 {
 }
 
-void Alfa::Dibujar(BufferedGraphics^ buffer, Bitmap^ bmp) {
+void Alfa::Dibujar(BufferedGraphics^ buffer, Bitmap^ bmp, Bitmap^ der) {
 	bmp->MakeTransparent(bmp->GetPixel(0, 0));
-	Rectangle porcion = Rectangle(ancho*indicex, indicey*alto, ancho, alto);
-	Rectangle aumento = Rectangle(x, y, ancho*2.5, alto*2.5);
-	buffer->Graphics->DrawImage(bmp, aumento, porcion, GraphicsUnit::Pixel);
+	der->MakeTransparent(der->GetPixel(0, 0));
+	if (dx < 0) {
+		ancho = 349 / 16;
+		Rectangle porcion = Rectangle(ancho * indicex, indicey * alto, ancho, alto);
+		Rectangle aumento = Rectangle(x, y, ancho * 2.5, alto * 2.5);
+		buffer->Graphics->DrawImage(bmp, aumento, porcion, GraphicsUnit::Pixel);
+	}
+
+	if (x + dx < 0 || x + dx + ancho >= 1079) {
+		dx *= -1;
+	}
+
+	if (dx > 0) {
+		ancho = 22;
+		Rectangle porcion = Rectangle(ancho * indicex, indicey * alto, ancho, alto);
+		Rectangle aumento = Rectangle(x, y, ancho * 2.5, alto * 2.5);
+		buffer->Graphics->DrawImage(der, aumento, porcion, GraphicsUnit::Pixel);
+	}
+
 	x += dx;
+
 }
 
-void Alfa::Mover(BufferedGraphics^ buffer, Bitmap^ izq) {
-	if (indicex >= 0 && indicex < 8)
-		indicex++;
-	else
-		indicex = 0;
-	Dibujar(buffer, izq);
+void Alfa::Mover(BufferedGraphics^ buffer, Bitmap^ izq, Bitmap^ der) {
+	if (dx < 0) {
+		if (indicex >= 0 && indicex < 8)
+			indicex++;
+		else
+			indicex = 0;
+	}
+	if (dx > 0) {
+		indicex == 15;
+		if (indicex <= 15 && indicex > 6)
+			indicex--;
+		else
+			indicex = 15;
+	}
+	Dibujar(buffer, izq, der);
 }
 
-Rectangle Alfa::getalfa(){ return Rectangle(x, y, ancho*2.5, alto*2.5); }
+Rectangle Alfa::getalfa() { return Rectangle(x, y, ancho * 2.5, alto * 2.5); }
 
 void Alfa::setvisible(bool v) { visible = v; }
 
